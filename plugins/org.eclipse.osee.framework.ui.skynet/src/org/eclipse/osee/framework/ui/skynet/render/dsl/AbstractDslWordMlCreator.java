1c9c4ea9a596c418017de6d88af1e00cc394c958
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2018-08-18 21:54:52 -0700

    refactor: Remove usage of deprecated artifact.getAttributes
    Change-Id: I7ad3e51611220bbe55c65552f6710bb82207fe6c

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/dsl/AbstractDslWordMlCreator.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/dsl/AbstractDslWordMlCreator.java
@@ -14,7 +14,6 @@
 package org.eclipse.osee.framework.ui.skynet.render.dsl;
 
 import org.eclipse.osee.framework.core.enums.PresentationType;
-import org.eclipse.osee.framework.skynet.core.artifact.Attribute;
 
 
 /**
@@ -75,7 +74,4 @@ public abstract class AbstractDslWordMlCreator {
 
       return textString;
    }
-
-   public abstract boolean isArtifactAttribute(Attribute<?> attribute);
-
 }
