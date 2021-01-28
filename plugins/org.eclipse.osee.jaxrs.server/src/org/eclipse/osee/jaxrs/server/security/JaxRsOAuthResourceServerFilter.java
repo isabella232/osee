eclipse-osee/org.eclipse.osee

feature[TW18520]: Update cxf to 3.3.9
Change-Id: Ib224f729813941cb511085a4d78321641babf16e
Signed-off-by: broberts <baily.e.roberts@boeing.com>

plugins/org.eclipse.osee.jaxrs.server/src/org/eclipse/osee/jaxrs/server/security/JaxRsOAuthResourceServerFilter.java
@@ -25,6 +25,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.apache.cxf.rs.security.oauth2.common.AccessTokenValidation;
@@ -71,7 +72,7 @@ public class JaxRsOAuthResourceServerFilter implements ContainerRequestFilter {
    }
 
    public void setAudiences(List<String> audiences) {
-      delegate.setAudiences(audiences);
+      delegate.setAudience(audiences.toString());
    }
 
    @Override
@@ -135,6 +136,11 @@ public class JaxRsOAuthResourceServerFilter implements ContainerRequestFilter {
             public AccessTokenValidation validateAccessToken(MessageContext mc, final String authScheme, final String accessToken) throws OAuthServiceException {
                return getRemoteTokenValidation(authScheme, accessToken);
             }
+
+            @Override
+            public AccessTokenValidation validateAccessToken(MessageContext mc, String authScheme, String accessToken, MultivaluedMap<String, String> values) throws OAuthServiceException {
+               return null;
+            }
          };
       }
 
@@ -164,6 +170,11 @@ public class JaxRsOAuthResourceServerFilter implements ContainerRequestFilter {
                   throw new OAuthServiceException("Error validating access token", ex.getCause());
                }
             }
+
+            @Override
+            public AccessTokenValidation validateAccessToken(MessageContext mc, String authScheme, String accessToken, MultivaluedMap<String, String> values) throws OAuthServiceException {
+               return null;
+            }
          };
       }
 
@@ -187,6 +198,10 @@ public class JaxRsOAuthResourceServerFilter implements ContainerRequestFilter {
       private static abstract class ClientAccessTokenValidator implements AccessTokenValidator {
 
          private WebTarget target;
+
+         public AccessTokenValidation validateAccessToken(MessageContext mc, String authScheme, String accessToken) throws OAuthServiceException {
+            return null;
+         }
 
          @Override
          public List<String> getSupportedAuthorizationSchemes() {

isabella232/osee

plugins/org.eclipse.osee.jaxrs.server/src/org/eclipse/osee/jaxrs/server/security/JaxRsOAuthResourceServerFilter.java
@@ -25,6 +25,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.apache.cxf.rs.security.oauth2.common.AccessTokenValidation;
@@ -71,7 +72,7 @@ public class JaxRsOAuthResourceServerFilter implements ContainerRequestFilter {
    }
 
    public void setAudiences(List<String> audiences) {
-      delegate.setAudiences(audiences);
+      delegate.setAudience(audiences.toString());
    }
 
    @Override
@@ -135,6 +136,11 @@ public class JaxRsOAuthResourceServerFilter implements ContainerRequestFilter {
             public AccessTokenValidation validateAccessToken(MessageContext mc, final String authScheme, final String accessToken) throws OAuthServiceException {
                return getRemoteTokenValidation(authScheme, accessToken);
             }
+
+            @Override
+            public AccessTokenValidation validateAccessToken(MessageContext arg0, String arg1, String arg2, MultivaluedMap<String, String> arg3) throws OAuthServiceException {
+               return null;
+            }
          };
       }
 
@@ -164,6 +170,11 @@ public class JaxRsOAuthResourceServerFilter implements ContainerRequestFilter {
                   throw new OAuthServiceException("Error validating access token", ex.getCause());
                }
             }
+
+            @Override
+            public AccessTokenValidation validateAccessToken(MessageContext arg0, String arg1, String arg2, MultivaluedMap<String, String> arg3) throws OAuthServiceException {
+               return null;
+            }
          };
       }
 
@@ -187,6 +198,10 @@ public class JaxRsOAuthResourceServerFilter implements ContainerRequestFilter {
       private static abstract class ClientAccessTokenValidator implements AccessTokenValidator {
 
          private WebTarget target;
+
+         public AccessTokenValidation validateAccessToken(MessageContext mc, String authScheme, String accessToken) throws OAuthServiceException {
+            return null;
+         }
 
          @Override
          public List<String> getSupportedAuthorizationSchemes() {